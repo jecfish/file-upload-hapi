446e79bbea8a4f6e70385873be24d043b7596061
chybie <chy.bie@htmail.com>
2017-02-10 09:30:06 +0800

    initial check in

---
+++ index.ts
@@ -0,0 +1,63 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as Loki from 'lokijs'
+import { imageFilter, loadCollection, cleanFolder } from './utils';
+
+// setup
+const DB_NAME = 'db.json';
+const COLLECTION_NAME = 'images';
+const UPLOAD_PATH = 'uploads';
+const upload = multer({ dest: `${UPLOAD_PATH}/`, fileFilter: imageFilter });
+const db = new Loki(`${UPLOAD_PATH}/${DB_NAME}`, { persistenceMethod: 'fs' });
+
+// optional: clean all data before start
+cleanFolder(UPLOAD_PATH);
+
+// app
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.post('/profile', upload.single('avatar'), async (req, res) => {
+    const col = await loadCollection(COLLECTION_NAME, db);
+    const data = col.insert(req.file);
+
+    db.saveDatabase();
+    res.send({ id: data.$loki, fileName: data.filename });
+})
+
+app.post('/photos/upload', upload.array('photos', 12), async (req, res) => {
+    const col = await loadCollection(COLLECTION_NAME, db)
+    let data = [];
+
+    (req.files as any).forEach(x => {
+        data = data.concat(col.insert(x));
+        db.saveDatabase();
+    })
+    res.send(data.map(x => ({ id: x.$loki, fileName: x.filename })));
+})
+
+app.get('/images', async (req, res) => {
+    const col = await loadCollection(COLLECTION_NAME, db);
+
+    res.send(col.data);
+})
+
+app.get('/images/:id', async (req, res) => {
+    const col = await loadCollection(COLLECTION_NAME, db);
+    const result = col.get(req.params.id);
+
+    if (!result) {
+        res.sendStatus(404);
+        return;
+    };
+    res.setHeader('Content-Type', result.mimetype);
+    fs.createReadStream(path.join(UPLOAD_PATH, result.filename)).pipe(res);
+})
+
+app.listen(3000, function () {
+    console.log('listening on port 3000!');
+})
---
+++ readme.md
@@ -0,0 +1,13 @@
+# File upload with Node, Hapi, Typescript
+
+1. Install [nodejs](https://nodejs.org/en/) (version 7.5+) and [yarn](https://yarnpkg.com/en/docs/install).
+2. Go to project directory, run `yarn`.
+3. Start the application, run `yarn start`.
+4. Go to `localhost:3001`
+
+## API End Points
+
+1. Upload an image via `localhost:3000/profile`, avatar field.
+2. Bulk Upload images via `localhost:3000/photos/upload`, photos field.
+3. View list of images via `localhost:3000/images`.
+4. Get a single image via `localhost:3000/images/{imageId}`.
